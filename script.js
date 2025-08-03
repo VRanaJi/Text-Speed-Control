c5c7a30efc7be9cd0a3156523784fe843ef36378
0fficial_coderr <bookmac1612@gmail.com>
2025-08-03 16:11:51 +0530

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const text = document.getElementById("text");
-const speedEl = document.getElementById("speed");
-const prog = "30 Days of JavaScript";
+const text = document.getElementById("text")
+const speedEl = document.getElementById("speed")
+const prog = "SUBSCRIBE The Channel- (0fficial_coderr)"
 
-let idx = 1;
-let speed = 300 / speedEl.value;
+let idx = 1
+let speed = 300 / speedEl.value
 
-writeText();
+writeText()
 
-function writeText() {
-    text.innerText = prog.slice(0, idx)
-	idx++;
-	
+function writeText(){
+	text.innerText = prog.slice(0, idx)
+	idx++
+
 	if(idx > prog.length){
-      idx = 1;
-    }
-	setTimeout(writeText, speed);
+		idx = 1
+	}
+	setTimeout(writeText, speed)
 }
 
-speedEl.addEventListener('input', (e) => {
-	speed = 300 / e.target.value;
-});
+speedEl.addEventListener('input', (e) =>{
+	speed = 300 / e.target.value
+})
